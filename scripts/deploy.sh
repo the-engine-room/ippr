9a6f71fe7026312898c0286b980b1111b25b2e33
Maya Richman <mayarichman@gmail.com>
2016-08-04 12:11:41 -0400

    remove secure field add ssh-agent call to remove call for passphrase

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ $TRAVIS_BRANCH == 'compiled-travis' ] ; then
 	echo "Deploying to remote"
+	eval `ssh-agent -s` #start shh agent
+	ssh-add ~/.ssh/id_rsa
 	cd _site
 	git add .
 	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
