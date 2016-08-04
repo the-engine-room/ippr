0719737e6861dd08d69f68baffc9c1dd73b1a516
Maya Richman <mayarichman@gmail.com>
2016-08-04 15:25:49 -0400

    test deploy git commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,10 +24,12 @@ if [ "$TRAVIS_BRANCH" = "master" ] ; then
 
   git config user.name "Travis CI"
   git config user.email "mayarichman+travisCI@gmail.com"
+  ls -la
   git add -A
   git status
   git commit -m "Deploy"
-  git push -f deploy compiled-travis
+  #git push -f deploy compiled-travis
+  git branch
 else
   echo "Not deploying, since this branch isn't master."
 fi
