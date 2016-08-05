3c183ca893cd80b2af1874fe0f064b6c5116d029
Maya Richman <mayarichman@gmail.com>
2016-08-05 12:42:23 -0400

    removed unneeded lines

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,26 +27,5 @@ function build {
 	cd ..
 }
 
-function compare {
-	echo "Comparing this build to the previous one"
-	cd _site
-	git checkout master
-
-	modfiles=$(git diff --name-only master..build | grep -v $gzip_ext)
-	modimg=$(grep $img_ext <<< "$modfiles" | tr '\n' ' ') # Not used right now, but this is a TODO.
-	modzopfli=$(grep $zopfli_ext <<< "$modfiles" | tr '\n' ' ')
-	modfiles=$(echo $modfiles | tr '\n' ' ')
-
-	git merge -X theirs --commit -m "Merge build #$TRAVIS_BUILD_NUMBER" build
-	cd ..
-}
-
 init
 build
-#compare
-
-# Compress assets with Zopfli (should always be the last command)
-#echo "Compressing the following assets using Zopfli: $modfiles"
-#cd _site
-#../zopfli/zopfli --i1000 $modzopfli
-#cd ..
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,50 +3,9 @@
 if [ $TRAVIS_BRANCH == 'master' ] ; then
 	echo "Deploying to remote"
 	cd _site
-  pwd
-	# git add -A
-	# git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
   git status
   git branch
   git push -f deploy master
 else
 	echo "Not deploying, since this branch isn't master."
 fi
-
-# if [ $TRAVIS_BRANCH == 'master' ] ; then
-# 	echo "Deploying to remote"
-#   pwd
-#   cd _site
-# #	git add -A
-# #	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-# 	git push deploy master
-# else
-# 	echo "Not deploying, since this branch isn't master."
-# fi
-
-#eval `ssh-agent -s` #start shh agent
-#ssh-add ~/.ssh/id_rsa
-
-# set -x
-
-# if [ "$TRAVIS_BRANCH" = "master" ] ; then
-#   cd _site
-# #  git init
-#   pwd
-#   git remote add deploy "deploy@45.55.35.212:/var/www/html/.git"
-#
-#   # verify remote
-#   git remote -v
-#
-#   git config --global user.name "Travis CI"
-#   git config --global user.email "mayarichman@gmail.com"
-#   cd ..
-#   ls -la
-#   git add -A
-#   git status
-#   git remote show origin
-#   git commit -m "Deploy"
-#   git push --force deploy master
-# else
-#   echo "Not deploying, since this branch isn't master."
-# fi
