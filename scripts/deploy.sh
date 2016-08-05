b12c7620682d905aaf21499205531a74c1dc91a1
Maya Richman <mayarichman@gmail.com>
2016-08-05 10:53:29 -0400

    testing simpler deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
-#if [ $TRAVIS_BRANCH == 'compiled-travis' ] ; then
-#	echo "Deploying to remote"
-#	cd _site
-#	git add .
-#	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-#	git push deploy compiled-travis
-#else
-#	echo "Not deploying, since this branch isn't compiled-travis."
-#fi
+if [ $TRAVIS_BRANCH == 'compiled-travis' ] ; then
+	echo "Deploying to remote"
+  pwd
+	git add -A
+	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
+	git push deploy compiled-travis
+else
+	echo "Not deploying, since this branch isn't compiled-travis."
+fi
 
 #eval `ssh-agent -s` #start shh agent
 #ssh-add ~/.ssh/id_rsa
 
-set -x
+# set -x
 
-if [ "$TRAVIS_BRANCH" = "master" ] ; then
-  cd _site
-#  git init
-  pwd
-#  git remote add deploy "deploy@45.55.35.212:/var/www/html/.git"
-
-  # verify remote
-  git remote -v
-
-  git config --global user.name "Travis CI"
-  git config --global user.email "mayarichman@gmail.com"
-  cd ..
-  ls -la
-  git add -A
-  git status
-  git remote show origin
-  git commit -m "Deploy"
-  git push --force deploy master
-else
-  echo "Not deploying, since this branch isn't master."
-fi
+# if [ "$TRAVIS_BRANCH" = "master" ] ; then
+#   cd _site
+# #  git init
+#   pwd
+#   git remote add deploy "deploy@45.55.35.212:/var/www/html/.git"
+#
+#   # verify remote
+#   git remote -v
+#
+#   git config --global user.name "Travis CI"
+#   git config --global user.email "mayarichman@gmail.com"
+#   cd ..
+#   ls -la
+#   git add -A
+#   git status
+#   git remote show origin
+#   git commit -m "Deploy"
+#   git push --force deploy master
+# else
+#   echo "Not deploying, since this branch isn't master."
+# fi
